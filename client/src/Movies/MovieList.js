3d116b257df035ae46b0e537c7bdfc4a58e1727a
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-02-25 14:51:09 -0800

    MVP completed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Link } from "react-router-dom";
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -34,8 +35,11 @@ export default class MovieList extends Component {
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
-    <div className="movie-card">
+    
+    <div className="movie-card"> 
+      <Link to={`/movies/${movie.id}`}>
       <h2>{title}</h2>
+      </Link>
       <div className="movie-director">
         Director: <em>{director}</em>
       </div>
